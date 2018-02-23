c45b8b5f1daab2d6f8b02c0cc6351c4fef4e0447
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-23 21:05:27 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,20 +13,20 @@ class EmailParser
   end
   
   def parse
-    emails.split.map do |email|
+    @emails.split.map do |email|
       email.split(',')
     end.flatten.uniq
   end
   
-  def parse
+  #def parse
     #email_array = emails.split(/[\s,]/) #split the strings and spit out an array, store in email_array
-    @emails.collect do |email_address| #loop through array
+    #@emails.collect do |email_address| #loop through array
       email_address.split(",")
      # if email_address != "" #remove the empty strings
     #  @@parse << email_address #add address to @@parse class variable (an array)
      # end 
-    end
+    #end
    #@@parse.uniq #returns array of only unique addresses
-  end 
+  #end 
   
 end 
