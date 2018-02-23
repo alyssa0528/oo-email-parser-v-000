33b09c3fb43d5edc67c27bf8bb66de4705b04882
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-23 21:14:11 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,6 @@ class EmailParser
   end
   
   def parse
-    binding.pry 
     @emails.split.map do |email| #take the string of emails, splits them into substrings at whitespace points, and throws them into an array; loops through that array
       email_array = email.split(',') #takes each email element in the array and further splits it at commas
     end
